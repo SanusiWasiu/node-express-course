fd31ac28b6d59cb971edc76aa413aa9f513eff34
SanusiWasiu <was.sanusi@gmail.com>
2021-12-27 00:24:32 +0100

    storeAPI changes

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,26 @@
+const Product = require('../models/product')
+
+const getAllProducts = async (req, res) => {
+    // throw new Error("testing async error")
+    const { featured, company } = req.query;
+    const queryObject = {}
+
+    if (featured){
+        queryObject.featured = featured === 'true'? true : false
+    }
+    if (company){
+        queryObject.company = company
+    }
+    const products = await Product.find( queryObject )
+    res.status(200).json({ products, nbHits: products.length })
+}
+
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({ featured: 'true' })
+    res.status(200).json({ products, nbHits: products.length })
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic,
+}
