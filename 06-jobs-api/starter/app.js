3cb086f5d9aed4c084f46903f59f2ecf20ad1e46
SanusiWasiu <was.sanusi@gmail.com>
2022-02-12 07:19:13 +0100

    jobs model created

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 // connectDB
 const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 // routers
 const authRouter = require('./routes/auth')
 const jobRouter = require('./routes/jobs')
@@ -19,7 +20,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/job', jobRouter)
+app.use('/api/v1/job', authenticateUser,jobRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 // const jwt = require('jsonwebtoken')
 // const bcrypt = require('bcryptjs')
 
@@ -20,7 +20,21 @@ const register = async (req, res) => {
 } 
 
 const login = async (req, res) => {
-    res.send("Login User")
+    // res.send("Login User")
+    const { email, password } = req.body
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({email})
+    if (!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = {
